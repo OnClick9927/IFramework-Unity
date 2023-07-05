c43621f2758a4f9de1f59f6fce624db3e476276a
wulei <2809399366@qq.com>
2023-07-05 11:44:21 +0800

    no message

--- Core/Runtime/.CS/UpdateCS.bat
+++ Core/Runtime/.CS/UpdateCS.bat
@@ -1,9 +1,31 @@
 echo off
 cd ../
-rd /s /q IFramework
-git clone -b src https://github.com/OnClick9927/IFramework
-cd IFramework
-rd /s /q .git
-cd ../
 
-pause
+if exist IFramework (
+    cd IFramework
+    if exist .src (
+       echo "begin pull IFramework from github"
+       md .git
+       xcopy /s /y /q .src .git
+       rd /s /q .src
+       git pull
+       md .src
+       xcopy /s /y /q .git .src
+       rd /s /q .git
+       cd ../
+    ) else (
+        cd ../
+        rd /s /q IFramework
+        goto IFramework
+    )
+) else (
+:IFramework
+    echo "begin clone IFramework from github"
+    git clone -b src https://github.com/OnClick9927/IFramework
+    cd IFramework
+    md .src
+    xcopy /s /y /q .git .src
+    rd /s /q .git
+    cd ../
+)
+echo "End IFramework"
