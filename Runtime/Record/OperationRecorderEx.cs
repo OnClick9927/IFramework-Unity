dd45368d13d3ee8545e2dda6fb7454efb062268f
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-08-01 13:54:24 +0800

    new

---
+++ Runtime/Record/OperationRecorderEx.cs
@@ -0,0 +1,67 @@
+﻿namespace IFramework.Record
+{
+    public static class OperationRecorderEx
+    {
+        /// <summary>
+        /// 分配命令
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static CommandState AllocateCommand(this Recorder t)
+        {
+            return t.Allocate<CommandState>();
+        }
+
+        /// <summary>
+        /// 分配命令组
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static CommandGroupState AllocateCommandGroup(this Recorder t)
+        {
+            return t.Allocate<CommandGroupState>();
+        }
+
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="redo"></param>
+        /// <param name="undo"></param>
+        /// <returns></returns>
+        public static T SetCommand<T>(this T t, IRecorderActor redo, IRecorderActor undo) where T : CommandState
+        {
+            t.SetValue(redo, undo);
+            return t;
+        }
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="redo"></param>
+        /// <param name="undo"></param>
+        /// <returns></returns>
+        public static T SetGroupCommand<T>(this T t, IRecorderActor redo, IRecorderActor undo) where T : CommandGroupState
+        {
+            t.SetValue(redo, undo);
+            return t;
+        }
+
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <param name="redo"></param>
+        /// <returns></returns>
+        public static T Subscribe<T>(this T t, bool redo = true) where T : BaseState
+        {
+            t.recorder.Subscribe(t, redo);
+            return t;
+        }
+    }
+}
